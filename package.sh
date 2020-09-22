c99e2cbcf82303883d239b80a6c3c369bb18d8ac
chirimenmonster <chirimenmonster@users.noreply.github.com>
2020-09-22 09:44:21 +0900

    add translate for damage log

--- package.sh
+++ package.sh
@@ -4,21 +4,27 @@ PYTHON2=python2
 TOOL=../wotmods-tools/wottool.py
 BUILDDIR=./build
 
+mkdir -p ${BUILDDIR}
+
+BASEDIR=.
 BASENAME=poliroid.battlehits_1.5.5
 EX_NAME=langpack_ja
-EX_VERSION=0.1
-
+EX_VERSION=0.2
+FILE_META=${BASEDIR}/meta.xml
+FILE_LICENSE=LICENSE
+FILE_RES=${BASEDIR}/res
 PACKNAME=${BASENAME}_${EX_NAME}_${EX_VERSION}.wotmod
 
-mkdir -p ${BUILDDIR}
-${PYTHON2} ${TOOL} wotmod -m meta.xml -m LICENSE -r res -o ${BUILDDIR}/${PACKNAME}
+${PYTHON2} ${TOOL} wotmod -m ${FILE_META} -m ${FILE_LICENSE} -r ${FILE_RES} -o ${BUILDDIR}/${PACKNAME}
 
 
 BASEDIR=modslistapi
 BASENAME=poliroid.modslistapi_1.3.3
 EX_NAME=langpack_ja
 EX_VERSION=0.1
-
+FILE_META=${BASEDIR}/meta.xml
+FILE_LICENSE=LICENSE
+FILE_RES=${BASEDIR}/res
 PACKNAME=${BASENAME}_${EX_NAME}_${EX_VERSION}.wotmod
 
-${PYTHON2} ${TOOL} wotmod -m ${BASEDIR}/meta.xml -m LICENSE -r ${BASEDIR}/res -o ${BUILDDIR}/${PACKNAME}
+${PYTHON2} ${TOOL} wotmod -m ${FILE_META} -m ${FILE_LICENSE} -r ${FILE_RES} -o ${BUILDDIR}/${PACKNAME}
